88f4e322625e53aae29849b183a710d6cacaf7b5
Gergely Imreh <imrehg@gmail.com>
2016-07-18 21:12:02 +0800

    expand scope

--- server.js
+++ server.js
@@ -2,61 +2,105 @@ var say = require('say'),
     FeedParser = require('feedparser'),
     request = require('request'),
     htmlToText = require('html-to-text'),
-    async = require('async');
+    async = require('async'),
+    moment = require('moment');
 
 var FEED_URL = process.env.FEED_URL;
 if (FEED_URL == undefined) {
     FEED_URL = "http://feeds.bbci.co.uk/news/world/rss.xml"
 }
 
-// Store feed texts to speak
-var texts = [];
+var RECHECK_PERIOD = process.env.RECHECK_PERIOD;
+if (RECHECK_PERIOD == undefined) {
+    RECHECK_PERIOD = 60 * 1000;   // 1 minute
+}
+
+var READ_OLD = process.env.READ_OLD;
+if (READ_OLD) {
+    READ_OLD = true;
+} else {
+    READ_OLD = false;
+}
+
+// // Store feed texts to speak
+var ITEMS = [],
+    LATEST = moment("20160101T000000"),
+    WORKQUEUE = async.queue(speakout, 1),
+    FIRSTRUN = true,
+    TIMEFORMAT = 'YYYY MMMM Do h:mma';
 
 //
-// Get feed, and store it in `texts`
+// Get feed, and send data to speech engine
 //
-var req = request(FEED_URL),
-    feedparser = new FeedParser();
+function getFeed() {
+    console.log("Checking at "+ moment().format(TIMEFORMAT));
+    var req = request(FEED_URL),
+	feedparser = new FeedParser();
 
-req.on('error', function (error) {
-    // handle any request errors
-});
-req.on('response', function (res) {
-    var stream = this;
+    req.on('error', function (error) {
+	// handle any request errors
+    });
+    req.on('response', function (res) {
+	var stream = this;
+	if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+	stream.pipe(feedparser);
+    });
 
-    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+    feedparser.on('error', function(error) {
+	// always handle errors
+    });
+    feedparser.on('readable', function() {
+	var stream = this
+	, meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+	, item;
 
-    stream.pipe(feedparser);
-});
+	while (item = stream.read()) {
+	    var date = moment(item.date);
+	    if (date <= LATEST) {
+		if (!FIRSTRUN) {
+		    continue
+		}
+	    } else {
+		LATEST = date;
+	    }
+	    // Formatting date, eg. "2016 July 18th 4:55pm",
+	    // which should be easy to pronounce by the speech engine
+	    var date = moment(item.date)
+	    var speechdate = date.format(TIMEFORMAT);
+	    var text = speechdate + " - " +
+		htmlToText.fromString(item.title, {"wordwrap": null}) + " - " +
+		htmlToText.fromString(item.description, {"wordwrap": null});
+	    console.log(text);
 
-feedparser.on('error', function(error) {
-    // always handle errors
-});
-feedparser.on('readable', function() {
-    // This is where the action is!
-    var stream = this
-    , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-    , item;
+	    if (READ_OLD || !FIRSTRUN) {
+		ITEMS.push(text);
+	    }
+	}
+    });
 
-    while (item = stream.read()) {
-	// console.log(item);
-    	// console.log(item.description);
-	var text = htmlToText.fromString(item.description);
-	// console.log(text);
-	texts.push(text);
-    }
-});
-// Fired when finished processing stream
-feedparser.on('end', () => {
-    console.log('There will be no more data.');
-    async.eachSeries(texts, speakout, function(err) { if (err) {console.log(err);} });
-});
+    // Fired once finished processing stream
+    feedparser.on('end', () => {
+	console.log('There will be no more data.');
+	// async.eachSeries(texts, speakout, function(err) { if (err) {console.log(err);} });
+	var numItems = ITEMS.length;
+	for (var i = 0; i < numItems; i++) {
+	    var lastfeeditem = ITEMS.pop();
+	    WORKQUEUE.push(lastfeeditem, function() { console.log("done!")});
+	}
+	if (FIRSTRUN) {
+	    FIRSTRUN = false;
+	}
+	setTimeout(getFeed, RECHECK_PERIOD);
+    });
+};
 
-//
-// Do the actual speak
-//
-var speakout = function(text, callback) {
+getFeed();
+
+function speakout(text, callback) {
     console.log("SPEAK => " + text);
+
+    callback();
+    return;
 
     // Festival cannot handle quite marks, clean it
     var textcleaned = text.replace(/["]/gi, '')
@@ -64,9 +108,9 @@ var speakout = function(text, callback) {
     // so might need to be more careful about what to pass it through `say`.
     // Find which voices are available by running `festival`, and starting
     // at `(voice_` press TAB to see what is available on the system.
-    say.speak(textcleaned, 'voice_ked_diphone', 1.0, function(err) {
+    say.speak(textcleaned, 'voice_kal_diphone', 1.0, function(err) {
 	if (err) {
-	    return console.error(err);
+	    console.log(err);
 	    callback(err);
 	}
 	console.log('Text has been spoken.');
